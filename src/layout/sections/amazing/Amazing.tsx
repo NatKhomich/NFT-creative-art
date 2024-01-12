2df2ed7822c019babb37dc16dbdcb77e63f3b295
NatKhomich <yalapulya94@mail.ru>
2024-01-12 18:17:22 +0600

    remove imports

--- src/layout/sections/amazing/Amazing.tsx
+++ src/layout/sections/amazing/Amazing.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Button} from '../../../components/button/Button';
 import {Card} from './card/Card';
-import {Title} from '../../../components/title/Title';
-import {Span} from '../../../components/span/Span';
 import {Container} from '../../../components/container/Container';
 import {Content} from './content/Content';
 
