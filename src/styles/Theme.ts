722558825dec4b2f1f38c170e097d958deb5f91b
NatKhomich <yalapulya94@mail.ru>
2024-01-15 12:08:39 +0600

    update Theme.ts media

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -8,12 +8,8 @@ export const theme = {
 
     },
     media: {
-        main: {
-            medium: 'screen and (max-width: 900px)',
-        },
-
         large: 'screen and (max-width: 1213px)',
-
+        medium: 'screen and (max-width: 900px)',
         tablet: 'screen and (max-width: 768px)',
         mobile: 'screen and (max-width: 576px)',
     },
